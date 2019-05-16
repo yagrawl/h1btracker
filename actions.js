a576b832a287447ad3408d8374e8a74d03bda9bb
Yash Agrawal <yagrawl2@gmail.com>
2019-05-15 22:18:37 -0700

    add premium processing status

--- actions.js
+++ actions.js
@@ -1,7 +1,8 @@
 exports.action = {
   "Case Was Received": "⏳ Hang tight.",
-  "Request for Additional Evidence Notice was Mailed": "⚠️ Email attorneys and confirm.",
-  "Response to USCIS’ Request for Evidence Was Received": "🎲 Laywers have responded. Hang tight.",
+  "Request for Additional Evidence Notice Was Mailed": "⚠️ Email attorneys and confirm.",
+  "Response To USCIS’ Request For Evidence Was Received": "🎲 Laywers have responded. Hang tight.",
   "Case Was Approved": "🎉 Yay! Book flight tickets!",
+  "Case Was Approved And My Decision Was Emailed": "🎉 Yay! Book flight tickets! [💲]",
   "Decision Notice Mailed": "🚫 FML."
 };
